10312ce9b4e60ed0842915ee5363ad6ab7702722
EmeraldWither <bikechamp007@gmail.com>
2022-04-19 18:57:44 -0400

    Configure proper build system
    It now allows for you to upload to your local mvn repo, and be able to reference it in your other projects.
    
    Also fixed OpenCV loading problems.

--- examples/Main.java
+++ examples/Main.java
@@ -1,29 +1,28 @@
-package org.emeraldcraft.djitello4j;
-
-import org.emeraldcraft.djitello4j.Tello.TelloClient;
+import org.emeraldcraft.djitello4j.tello.TelloClient;
 
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         TelloClient drone = new TelloClient();
         Scanner scanner = new Scanner(System.in);
         boolean isConnected = drone.connect();
 
-        if(!isConnected){
+        if (!isConnected) {
             drone.disconnect();
             return;
         }
         //ask user if they want to fly
         System.out.println("Do you want to fly? (y/n)");
-        String answer = scanner.nextLine();        if (answer.equals("y")) {
+        String answer = scanner.nextLine();
+        if (answer.equals("y")) {
             drone.setSpeed(40);
             drone.takeoff();
             drone.moveForward(50);
             drone.moveBackward(50);
             drone.land();
-        }
-        else {
+        } else {
             System.out.println("Goodbye!");
         }
         drone.disconnect();
--- src/main/java/org/emeraldcraft/djitello4j/tello/TelloClient.java
+++ src/main/java/org/emeraldcraft/djitello4j/tello/TelloClient.java
@@ -1,4 +1,6 @@
-package org.emeraldcraft.djitello4j.Tello;
+package org.emeraldcraft.djitello4j.tello;
+
+import nu.pattern.OpenCV;
 
 import java.io.IOException;
 import java.net.*;
@@ -10,6 +12,9 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import static org.emeraldcraft.djitello4j.tello.TelloClient.Log.debug;
+import static org.emeraldcraft.djitello4j.tello.TelloClient.Log.error;
+
 /**
  * Represents the Tello Drone. This class is responsible for sending and receiving data from the Tello.
  * The battery and state information is retrieved from {@link TelloStateReceiver}
@@ -17,6 +22,9 @@ import java.util.concurrent.TimeUnit;
  * @author EmerqldWither
  */
 public class TelloClient {
+    static {
+        OpenCV.loadShared();
+    }
     private long timeOfLastCommand = 0;
     private boolean canRestart = true;
     private boolean hasConnected = false;
@@ -388,6 +396,105 @@ public class TelloClient {
             String currentTime;
             currentTime = formatter.format(date);
             System.out.println("<!><!> [ERROR " + currentTime + "]: " + msg +  " <!><!>");
+        }
+    }
+
+    /**
+     * This class is used to receive the state of the Tello drone via the UDP socket.
+     * This information is passed to provide information about the battery, tof, temperature, and height.
+     * This runs in the background on a separate thread, and the information is sent from the Tello drone automatically.
+     *
+     * This is an internal class, and should not be used directly.
+     */
+    static class TelloStateReceiver extends Thread {
+        private DatagramSocket socket;
+        private final byte[] buf = new byte[256];
+        private boolean running = false;
+
+        private int currentBattery = 0;
+        private int currentTemp = 0;
+        private int height = 0;
+        private int tof = 0;
+
+        public TelloStateReceiver() throws SocketException {
+            try {
+                socket = new DatagramSocket(8890, InetAddress.getByName("0.0.0.0"));
+                socket.setSoTimeout(2000);
+                //Try checking to see if we are online and running
+                if (isOnline()) {
+                    running = true;
+                    //Receive 1st state packet
+                    this.receiveAndParsePackets();
+                    debug("Tello state receiver started");
+                }
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void run() {
+            while (running) {
+                receiveAndParsePackets();
+            }
+        }
+        public void receiveAndParsePackets() {
+            try {
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+                String message = new String(packet.getData(), 0, packet.getLength());
+                parseResponse(message);
+            } catch (IOException e) {
+                if(!running) return;
+                error("Error receiving packet from Tello");
+                e.printStackTrace();
+            }
+        }
+        private void parseResponse(String response){
+            String[] data = response.split(";");
+            for(String s : data){
+                if(s.isBlank()) continue;
+
+                String dataType = s.split(":")[0];
+                String dataValue = s.split(":")[1];
+                switch (dataType) {
+                    case "bat" -> this.currentBattery = Integer.parseInt(dataValue);
+                    case "temp" -> this.currentTemp = Integer.parseInt(dataValue);
+                    case "h" -> this.height = Integer.parseInt(dataValue);
+                    case "tof" -> this.tof = Integer.parseInt(dataValue);
+                }
+            }
+        }
+        public void stopRunning() {
+            running = false;
+        }
+        public boolean isOnline(){
+            try{
+                DatagramPacket packet
+                        = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+                return true;
+            }
+            catch(IOException exception){
+                return false;
+            }
+        }
+
+        public int getCurrentBattery() {
+            return currentBattery;
+        }
+
+        public int getCurrentTemp() {
+            return currentTemp;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public int getTof() {
+            return tof;
         }
     }
 }
