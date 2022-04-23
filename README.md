9ae8aeaa8ddfabb21e370b383c279252ad9ccc5d
EmeraldWither <bikechamp007@gmail.com>
2022-04-23 12:31:35 -0400

    Add opencv camera streaming

---
+++ README.md
@@ -0,0 +1,6 @@
+OpenCV
+-------------------
+
+This branch uses FFmpeg to decode video files, and then convert the frames to OpenCV format.
+
+Because of the large file size for these dependencies (80MB!), it is suggested not to use this version unless you actually want to use these features
--- src/main/java/org/emeraldcraft/djitello4j/tello/TelloClient.java
+++ src/main/java/org/emeraldcraft/djitello4j/tello/TelloClient.java
@@ -1,6 +1,10 @@
 package org.emeraldcraft.djitello4j.tello;
 
-import nu.pattern.OpenCV;
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.opencv.opencv_java;
+import org.opencv.core.Mat;
+import org.opencv.highgui.HighGui;
 
 import java.io.IOException;
 import java.net.*;
@@ -12,19 +16,21 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import static org.emeraldcraft.djitello4j.tello.TelloClient.Log.debug;
-import static org.emeraldcraft.djitello4j.tello.TelloClient.Log.error;
+import static org.bytedeco.javacv.OpenCVFrameConverter.ToOrgOpenCvCoreMat;
+import static org.emeraldcraft.djitello4j.tello.TelloClient.Log.*;
 
 /**
  * Represents the Tello Drone. This class is responsible for sending and receiving data from the Tello.
  * The battery and state information is retrieved from {@link TelloStateReceiver}
- * @see TelloStateReceiver
+ *
  * @author EmerqldWither
+ * @see TelloStateReceiver
  */
 public class TelloClient {
     static {
-        OpenCV.loadShared();
+        Loader.load(opencv_java.class);
     }
+
     private long timeOfLastCommand = 0;
     private boolean canRestart = true;
     private boolean hasConnected = false;
@@ -34,19 +40,20 @@ public class TelloClient {
     private final int port = 8889;
     private ScheduledExecutorService keepAliveExecutor;
     private TelloStateReceiver receiver;
+    private TelloCameraStream stream;
 
-    public TelloClient(){
-        try{
+    private boolean isStreamOn = false;
+
+    public TelloClient() {
+        try {
             socket = new DatagramSocket(port);
             address = InetAddress.getByName("192.168.10.1");
             receiver = new TelloStateReceiver();
-        }
-        catch (SocketException | UnknownHostException e){
-            if(e instanceof SocketException){
+        } catch (SocketException | UnknownHostException e) {
+            if (e instanceof SocketException) {
                 Log.error("There was an error while creating the socket! (Is the program already running?)");
                 e.printStackTrace();
-            }
-            else {
+            } else {
                 Log.error("There was an error while finding the address (192.168.10.1)! (Is the Tello connected and is bypassing the firewall?)");
                 e.printStackTrace();
             }
@@ -54,38 +61,39 @@ public class TelloClient {
     }
 
     public static class FlipDirection {
+
         public static final String FORWARD = "f";
         public static final String BACKWARD = "b";
         public static final String LEFT = "l";
         public static final String RIGHT = "r";
     }
-
     /**
      * Connect to the drone. This method will block until the connection is established.
+     *
      * @return True if the Tello has connected, or, is already connected. False if the Tello has failed to connect.
      */
     public boolean connect() {
         //First check to see if we are receiving state packets
         if (hasConnected) return true;
-        if(!canRestart) return false;
+        if (!canRestart) return false;
         receiver.start();
         if (receiver.isOnline()) {
             startKeepAliveThread();
             receiver.receiveAndParsePackets();
             hasConnected = true;
 
-            Log.debug("The TelloStateReceiver is alive and receiving packets. Marked as running.");
-            Log.debug("Tello is connected, battery is " + getBattery() + "%");
+            debug("The TelloStateReceiver is alive and receiving packets. Marked as running.");
+            debug("Tello is connected, battery is " + getBattery() + "%");
             return true;
         }
-        Log.debug("Receiver is dead. Trying to reconnect...");
+        debug("Receiver is dead. Trying to reconnect...");
         String response = sendPacket("command", "ok", 5_000);
         if (response != null) {
             startKeepAliveThread();
             receiver.receiveAndParsePackets();
             hasConnected = true;
-            Log.debug("Tello is connected via a new connection");
-            Log.debug("Tello is connected, battery is " + getBattery() + "%");
+            debug("Tello is connected via a new connection");
+            debug("Tello is connected, battery is " + getBattery() + "%");
             return true;
         }
         Log.error("Tello is not connected. Please check the Tello's connection and try again.\n" +
@@ -101,16 +109,62 @@ public class TelloClient {
 
     /**
      * Sends a keep alive packet to keep the drone connected even if no commands are sent.
+     * The keep alive packet is sent every 10 seconds automatically, so you shouldn't usually be calling this method.
      */
     public void sendKeepAlivePacket() {
         if (System.currentTimeMillis() - this.timeOfLastCommand > 13_000) {
             String response = sendPacket("alive", null, 3_000);
             if (response == null) {
-                System.out.println("Failed to send keep-alive packet");
+                debug("Failed to send keep-alive packet");
                 return;
             }
-            Log.debug("Sent keep-alive packet");
+            debug("Sent keep-alive packet");
         }
+    }
+
+    private void startCameraStream() {
+        this.stream = new TelloCameraStream(this);
+        this.stream.start();
+    }
+
+    /**
+     * Enables the camera stream.
+     */
+    public void streamOn() {
+        if (this.isStreamOn) return;
+        sendKeepAlivePacket();
+        if (sendPacket("streamon", "ok", 15_000) != null) {
+            this.isStreamOn = true;
+            startCameraStream();
+        }
+    }
+
+    public boolean isStreamOn() {
+        return isStreamOn;
+    }
+
+    /**
+     * Disables the camera stream.
+     */
+    public void streamOff() {
+        if (!this.isStreamOn) return;
+        sendKeepAlivePacket();
+        if (sendPacket("streamoff", "ok", 15_000) != null) {
+            this.isStreamOn = false;
+            this.stream.stopFetchingFrames();
+        }
+    }
+
+    /**
+     * This will return the current frame from the camera stream as an {@link Mat} object.
+     * Please ensure that you call {@link #streamOn} before calling this method.
+     *
+     * You can use {@link HighGui#imshow(String, Mat)} to display the image.
+     *
+     * @return The frame from the camera stream.
+     */
+    public Mat getFrame() {
+        return stream.getFrame();
     }
 
 
@@ -121,10 +175,10 @@ public class TelloClient {
         sendKeepAlivePacket();
         String response = sendPacket("takeoff", "ok", 20_000);
         if (response != null) {
-            System.out.println("Tello took off");
+            info("Tello took off");
             return;
         }
-        System.out.println("Failed to takeoff");
+        info("Failed to takeoff");
     }
 
     /**
@@ -142,6 +196,7 @@ public class TelloClient {
 
     /**
      * Moves the drone forward X amount in CM.
+     *
      * @param amount The amount of cm to move the drone forward from 20cm to 500cm.
      */
     public void moveForward(int amount) {
@@ -154,6 +209,7 @@ public class TelloClient {
 
     /**
      * Moves the drone backward X amount in CM.
+     *
      * @param amount Move the drone backward X amount in CM from 20cm to 500cm.
      */
     public void moveBackward(int amount) {
@@ -167,6 +223,7 @@ public class TelloClient {
 
     /**
      * Moves the drone right X amount in CM.
+     *
      * @param amount The amount of cm to move the drone right from 20cm to 500cm.
      */
     public void moveRight(int amount) {
@@ -180,6 +237,7 @@ public class TelloClient {
 
     /**
      * Moves the drone left X amount in CM.
+     *
      * @param amount The amount of cm to move the drone left from 20cm to 500cm.
      */
     public void moveLeft(int amount) {
@@ -193,6 +251,7 @@ public class TelloClient {
 
     /**
      * Moves the drone up X amount in CM.
+     *
      * @param amount The amount of cm to move the drone up from 20cm to 500cm.
      */
     public void moveUp(int amount) {
@@ -206,6 +265,7 @@ public class TelloClient {
 
     /**
      * Moves the drone down X amount in CM.
+     *
      * @param amount The amount of cm to move the drone down from 20cm to 500cm.
      */
     public void moveDown(int amount) {
@@ -219,6 +279,7 @@ public class TelloClient {
 
     /**
      * Rotates the drone clockwise X amount in degrees.
+     *
      * @param amount The amount of degrees to rotate the drone clockwise from 0 to 3600.
      */
     public void rotateCW(int amount) {
@@ -232,6 +293,7 @@ public class TelloClient {
 
     /**
      * Rotates the drone counter-clockwise X amount in degrees.
+     *
      * @param amount The amount of degrees to rotate the drone counter-clockwise from 0 to 3600.
      */
     public void rotateCWW(int amount) {
@@ -245,9 +307,10 @@ public class TelloClient {
 
     /**
      * Moves the drone emulating a controller.
-     * @param lr The left-right value of the drone.
-     * @param fb The forward-backward value of the drone.
-     * @param ud The up-down value of the drone.
+     *
+     * @param lr  The left-right value of the drone.
+     * @param fb  The forward-backward value of the drone.
+     * @param ud  The up-down value of the drone.
      * @param yaw The yaw value of the drone.
      */
     public void sendRCControl(int lr, int fb, int ud, int yaw) {
@@ -266,7 +329,7 @@ public class TelloClient {
      *
      * @param direction The direction to flip in
      */
-    public void flip(String direction)  {
+    public void flip(String direction) {
         sendKeepAlivePacket();
         sendPacket("flip " + direction, "ok", 20_000);
     }
@@ -275,7 +338,7 @@ public class TelloClient {
      * Emergency stops the drone. The drone will also disconnect and cannot be reconnected.
      * In order to reconnect you will need to restart the drone.
      */
-    public void emergency(){
+    public void emergency() {
         sendKeepAlivePacket();
         sendPacket("emergency", null, -1);
         this.disconnect();
@@ -284,9 +347,10 @@ public class TelloClient {
 
     /**
      * Set the speed of the drone from 10 to 100.
+     *
      * @param speed The speed to set the drone to.
      */
-    public void setSpeed(int speed)  {
+    public void setSpeed(int speed) {
         sendKeepAlivePacket();
 
         if (speed > 100) throw new IllegalArgumentException("Speed must be less than 100");
@@ -302,6 +366,7 @@ public class TelloClient {
 
     /**
      * Retrieves the current battery percentage from @{link {@link TelloStateReceiver#getCurrentBattery()}}
+     *
      * @return The batter in %.
      */
     public int getBattery() {
@@ -317,11 +382,12 @@ public class TelloClient {
 
     /**
      * Internal method to send a packet to the drone.
+     *
      * @return The response from the Tello.
      */
-    private String sendPacket(String command, String expectedResponse, int timeout){
+    private String sendPacket(String command, String expectedResponse, int timeout) {
         if (!isEnabled) return null;
-        try{
+        try {
             socket.setSoTimeout(timeout);
             int attempts = 0;
             while (attempts < 5) {
@@ -340,7 +406,7 @@ public class TelloClient {
                     if (expectedResponse == null) return received;
 
                     if (expectedResponse.equalsIgnoreCase(received)) {
-                        Log.debug("Got successful (expected) response: \"" + received + "\" for command: \"" + command + "\"");
+                        debug("Got successful (expected) response: \"" + received + "\" for command: \"" + command + "\"");
                         return received;
                     }
                     Log.error("Got unsuccessful packet: \"" + received + "\" for command \"" + command + "\"");
@@ -351,8 +417,7 @@ public class TelloClient {
                 }
             }
             Log.error("Socket connection timed out after 5 attempts for command: \"" + command + "\" with an expected response \"" + expectedResponse + "\" with a timeout: " + timeout + "ms | GIVING UP!!!");
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             Log.error("IOException while sending packet: \"" + command + "\" with an expected response \"" + expectedResponse + "\" with a timeout: " + timeout + "ms | GIVING UP!!!");
         }
         return null;
@@ -367,16 +432,20 @@ public class TelloClient {
         }
         receiver.stopRunning();
         socket.disconnect();
+        this.stream.stopFetchingFrames();
+        HighGui.destroyAllWindows();
         System.out.println("Tello disconnected. Goodbye!");
     }
+
     private int calcSpeedTime(int amount) {
         return ((amount / 15) + 3) * 1000;
     }
+
     public static class Log {
         private static final boolean IS_DEBUGGING = true;
 
-        public static void debug(String msg){
-            if(!IS_DEBUGGING) return;
+        public static void debug(String msg) {
+            if (!IS_DEBUGGING) return;
 
             Date date = new Date();
             DateFormat formatter;
@@ -387,7 +456,8 @@ public class TelloClient {
             currentTime = formatter.format(date);
             System.out.println("[DEBUG " + currentTime + "]: " + msg);
         }
-        public static void error(String msg){
+
+        public static void error(String msg) {
             Date date = new Date();
             DateFormat formatter;
             formatter = new SimpleDateFormat("h:mm:ss a");
@@ -395,7 +465,18 @@ public class TelloClient {
             formatter.setTimeZone(TimeZone.getDefault());
             String currentTime;
             currentTime = formatter.format(date);
-            System.out.println("<!><!> [ERROR " + currentTime + "]: " + msg +  " <!><!>");
+            System.out.println("<!><!> [ERROR " + currentTime + "]: " + msg + " <!><!>");
+        }
+
+        public static void info(String msg) {
+            Date date = new Date();
+            DateFormat formatter;
+            formatter = new SimpleDateFormat("h:mm:ss a");
+
+            formatter.setTimeZone(TimeZone.getDefault());
+            String currentTime;
+            currentTime = formatter.format(date);
+            System.out.println("[INFO " + currentTime + "]: " + msg);
         }
     }
 
@@ -403,10 +484,10 @@ public class TelloClient {
      * This class is used to receive the state of the Tello drone via the UDP socket.
      * This information is passed to provide information about the battery, tof, temperature, and height.
      * This runs in the background on a separate thread, and the information is sent from the Tello drone automatically.
-     *
+     * <p>
      * This is an internal class, and should not be used directly.
      */
-    static class TelloStateReceiver extends Thread {
+    static final class TelloStateReceiver extends Thread {
         private DatagramSocket socket;
         private final byte[] buf = new byte[256];
         private boolean running = false;
@@ -439,6 +520,7 @@ public class TelloClient {
                 receiveAndParsePackets();
             }
         }
+
         public void receiveAndParsePackets() {
             try {
                 DatagramPacket packet = new DatagramPacket(buf, buf.length);
@@ -446,15 +528,16 @@ public class TelloClient {
                 String message = new String(packet.getData(), 0, packet.getLength());
                 parseResponse(message);
             } catch (IOException e) {
-                if(!running) return;
+                if (!running) return;
                 error("Error receiving packet from Tello");
                 e.printStackTrace();
             }
         }
-        private void parseResponse(String response){
+
+        private void parseResponse(String response) {
             String[] data = response.split(";");
-            for(String s : data){
-                if(s.isBlank()) continue;
+            for (String s : data) {
+                if (s.isBlank()) continue;
 
                 String dataType = s.split(":")[0];
                 String dataValue = s.split(":")[1];
@@ -466,17 +549,18 @@ public class TelloClient {
                 }
             }
         }
+
         public void stopRunning() {
             running = false;
         }
-        public boolean isOnline(){
-            try{
+
+        public boolean isOnline() {
+            try {
                 DatagramPacket packet
                         = new DatagramPacket(buf, buf.length);
                 socket.receive(packet);
                 return true;
-            }
-            catch(IOException exception){
+            } catch (IOException exception) {
                 return false;
             }
         }
@@ -495,6 +579,72 @@ public class TelloClient {
 
         public int getTof() {
             return tof;
+        }
+    }
+
+    static class TelloCameraStream extends Thread {
+        private boolean isRunning = true;
+
+        private Mat frame = new Mat();
+        private final TelloClient client;
+        ToOrgOpenCvCoreMat converter = new ToOrgOpenCvCoreMat();
+        private FFmpegFrameGrabber frameGrabber;
+
+        public TelloCameraStream(TelloClient client){
+            this.client = client;
+
+            try {
+                //Create a new FFmpegFrameGrabber to grab the video stream from the Tello stream
+                frameGrabber = new FFmpegFrameGrabber("udp://@0.0.0.0:11111");
+                frameGrabber.start();
+
+            } catch (FFmpegFrameGrabber.Exception e) {
+                error("Error starting frame grabber");
+                e.printStackTrace();
+            }
+
+            if (!frameGrabber.hasVideo()) {
+                error("Failed to open camera");
+                return;
+            }
+            info("Tello camera stream started");
+        }
+
+        public void run() {
+            while (isRunning && client.isStreamOn) {
+                fetchFrame();
+            }
+        }
+
+        private void fetchFrame() {
+
+            try {
+                //Convert the frame to a Mat
+                frame = converter.convert(frameGrabber.grab());
+            } catch (FFmpegFrameGrabber.Exception e) {
+                error("Error fetching frame from grabber");
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * @return Returns the decoded frame as a string
+         */
+        public Mat getFrame() {
+            return frame;
+        }
+
+        public void stopFetchingFrames() {
+            isRunning = false;
+            try {
+                frameGrabber.stop();
+            } catch (FFmpegFrameGrabber.Exception e) {
+                error("Error stopping frame grabber");
+            }
+        }
+
+        public boolean isRunning() {
+            return isRunning;
         }
     }
 }
